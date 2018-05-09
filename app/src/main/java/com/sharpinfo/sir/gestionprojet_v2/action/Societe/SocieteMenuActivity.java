6883c244b9d4125d4dee05129695f333d51072b7
Boss <htakouit@gmail.com>
2018-05-09 10:53:03 +0100

    added menu in societe

---
+++ app/src/main/java/com/sharpinfo/sir/gestionprojet_v2/action/Societe/SocieteMenuActivity.java
@@ -0,0 +1,53 @@
+package com.sharpinfo.sir.gestionprojet_v2.action.Societe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+import com.sharpinfo.sir.gestionprojet_v2.R;
+import com.sharpinfo.sir.gestionprojet_v2.action.MainActivity;
+
+import helper.Dispacher;
+
+public class SocieteMenuActivity extends AppCompatActivity implements View.OnClickListener {
+    private CardView projetCard;
+    private CardView societeCard;
+    private CardView depenseCard;
+    private CardView tacheCard;
+    private CardView statistiqueCard;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_societe_menu);
+        projetCard = (CardView) findViewById(R.id.projetCardView);
+        societeCard = (CardView) findViewById(R.id.societeCardView);
+        projetCard.setOnClickListener(this);
+        societeCard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.societeCardView:
+                Dispacher.forward(SocieteMenuActivity.this, SocieteCreateActivity.class);
+                break;
+            case R.id.projetCardView:
+                Dispacher.forward(SocieteMenuActivity.this, MainActivity.class);
+                break;
+                default:break;
+        }
+
+
+    }
+
+    public void goToProjets(View view) {
+        projetCard = (CardView) findViewById(R.id.projetCardView);
+        Dispacher.forward(SocieteMenuActivity.this, SocieteCreateActivity.class);
+    }
+
+
+}
